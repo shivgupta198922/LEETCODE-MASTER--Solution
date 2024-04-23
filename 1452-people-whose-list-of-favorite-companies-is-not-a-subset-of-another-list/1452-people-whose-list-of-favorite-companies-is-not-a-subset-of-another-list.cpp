262d2ed726fe0b0e0c2dcb1e4fb7b99556d155cd
shivgupta198922 <153531902+shivgupta198922@users.noreply.github.com>
2024-04-23 20:36:57 +0530

    Time: 551 ms (46.88%), Space: 58.4 MB (93.13%) - LeetHub

---
+++ 1452-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/1452-people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> peopleIndexes(vector<vector<string>>& com) {
+        int n = com.size();
+        vector<int>ans;
+        for(auto &cm:com)sort(begin(cm),end(cm));
+        for(int i=0;i<n;i++){
+            bool f=false;
+            for(int j=0;j<n&&!f;j++){
+                if(i!=j)
+                    f = includes(begin(com[j]),end(com[j]),begin(com[i]),end(com[i]));
+            }
+            if(!f)ans.push_back(i);
+        }
+        return ans;
+    }
+};
