8c383dedfca3725e58ae594c65d271935b31dacf
shivgupta198922 <153531902+shivgupta198922@users.noreply.github.com>
2024-05-29 22:05:33 +0530

    Time: 0 ms (100%), Space: 7.6 MB (47.06%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,26 +1,15 @@
 class Solution {
- public:
-  int numSteps(string s) {
-    int ans = 0;
-
-    // All the trailing 0s can be popped by 1 step.
-    while (s.back() == '0') {
-      s.pop_back();
-      ++ans;
+public:
+    int numSteps(string s) {
+        int carry = 0, steps = 0;
+        for(int i = s.length() - 1; i > 0; i--) {
+            if((s[i] - '0') + carry == 1) {
+                steps += 2;
+                carry = 1;
+            } else {
+                steps++;
+            }
+        }
+        return steps + carry;
     }
-
-    if (s == "1")
-      return ans;
-
-    // `s` is now odd, so add 1 to `s` and cost 1 step.
-    ++ans;
-
-    // All the 1s will become 0s and can be popped by 1 step.
-    // All the 0s will become 1s and can be popped by 2 steps (adding 1 then
-    // dividing by 2).
-    for (const char c : s)
-      ans += c == '1' ? 1 : 2;
-
-    return ans;
-  }
 };
