6843b4e34f30b000240702f1b1b07bf852e891b9
shivgupta198922 <153531902+shivgupta198922@users.noreply.github.com>
2024-05-30 18:32:06 +0530

    Time: 4 ms (54.79%), Space: 9.1 MB (49.44%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,19 +1,14 @@
-class Solution:
-  def countTriplets(self, arr: List[int]) -> int:
-    ans = 0
-    xors = [0]
-    prefix = 0
-
-    for i, a in enumerate(arr):
-      prefix ^= a
-      xors.append(prefix)
-
-    for j in range(1, len(arr)):
-      for i in range(0, j):
-        xors_i = xors[j] ^ xors[i]
-        for k in range(j, len(arr)):
-          xors_k = xors[k + 1] ^ xors[j]
-          if xors_i == xors_k:
-            ans += 1
-
-    return ans
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        int count = 0;
+        for(int i = 0; i < arr.size() - 1; i++) {
+            int val = arr[i];
+            for(int k = i + 1; k < arr.size(); k++) {
+                val ^= arr[k];
+                if(val == 0) count += (k - i);
+            }
+        }
+        return count;
+    }
+};
