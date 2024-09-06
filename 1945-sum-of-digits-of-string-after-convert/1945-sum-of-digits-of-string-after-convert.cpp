178f9a3c9924555dd4495a9068f31fc3ca6dc9ba
shivgupta198922 <153531902+shivgupta198922@users.noreply.github.com>
2024-09-06 12:10:43 +0530

    Time: 0 ms (100%), Space: 8 MB (82.21%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     int getLucky(string s, int k) {
-        int ans=convert(s);
-        for(int i=1;i<k;i++)
+        int ans = convert(s);
+        for(int i=1; i<k; i++)
         {
             ans=sumofdigit(ans);
+            
         }
-        
-        return ans;
+        return ans;    
     }
     int convert(string s)
     {
         int sum=0;
-        for(char c:s)
+        for(char c :s)
         {
-            int value= c-'a' + 1;
-            sum += value < 10 ? value: (value%10 + value/10);
+            int value = c-'a'+1;
+            sum += value < 10 ? value :(value %10 + value/10);  
         }
         return sum;
     }
     int sumofdigit(int num)
     {
-        int digitsum=0;
+        int sum=0;
         while(num>0)
         {
-            digitsum+= num%10;
-            num/=10;
+        sum+=num%10;
+        num=num/10;
         }
-        return digitsum;
+        return sum;
+
     }
+
 };
