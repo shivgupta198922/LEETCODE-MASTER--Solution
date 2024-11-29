05ce7526fa34788b48da5501d2617e7fac56de0e
shivgupta198922 <153531902+shivgupta198922@users.noreply.github.com>
2024-11-29 22:58:00 +0530

    Time: 0 ms (100%), Space: 14.3 MB (20.08%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,28 +1,30 @@
 class Solution {
- public:
-  vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-    vector<vector<int>> ans;
-    ranges::sort(candidates);
-    dfs(candidates, 0, target, {}, ans);
-    return ans;
-  }
-
- private:
-  void dfs(const vector<int>& A, int s, int target, vector<int>&& path,
-           vector<vector<int>>& ans) {
-    if (target < 0)
-      return;
-    if (target == 0) {
-      ans.push_back(path);
-      return;
+public:
+    int n;
+    vector<int>nums, path;
+    vector<vector<int>>ans;
+    void backtracking(int target, int start)
+    {
+        if(target == 0)
+        {
+            ans.push_back(path);
+            return;
+        }
+        for(int i=start;i<n;i++)
+        {
+            if(nums[i] > target) break;
+            if(i > start and nums[i] == nums[i-1]) continue;
+            path.push_back(nums[i]);
+            backtracking(target - nums[i], i+1);
+            path.pop_back();
+        }
     }
-
-    for (int i = s; i < A.size(); ++i) {
-      if (i > s && A[i] == A[i - 1])
-        continue;
-      path.push_back(A[i]);
-      dfs(A, i + 1, target - A[i], move(path), ans);
-      path.pop_back();
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) 
+    {
+        sort(begin(candidates), end(candidates));
+        nums = candidates;
+        n = nums.size();
+        backtracking(target, 0);
+        return ans;
     }
-  }
 };
