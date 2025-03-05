90c9e92266ec2dc0a732574809314eb680d9659a
shivgupta198922 <153531902+shivgupta198922@users.noreply.github.com>
2025-03-05 16:51:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c); 
+            } else {
+                
+                if (stack.isEmpty() || 
+                   (c == ')' && stack.pop() != '(') || 
+                   (c == '}' && stack.pop() != '{') || 
+                   (c == ']' && stack.pop() != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty(); 
+    }
+}
