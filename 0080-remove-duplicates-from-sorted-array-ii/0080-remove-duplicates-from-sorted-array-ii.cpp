de487094f57f17e791a91528d7e923f68ad56237
shivgupta198922 <153531902+shivgupta198922@users.noreply.github.com>
2025-09-26 22:04:31 +0530

    Time: 7 ms (73.51%), Space: 19.3 MB (95.1%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0;
+        int n=nums.size();
+        int k=2; 
+        if(n<=2) return n;
 
-        int k = 0;
-        int n = nums.size();
-
-        for (int i = 0; i < n; ) {
-            // check if there are at least 2 elements left and first two are same
-            if (i + 1 < n && nums[i] == nums[i + 1]) {
-                // if third element exists and is same, keep only two
-                nums[k] = nums[i];
-                nums[k + 1] = nums[i + 1];
-                k += 2;
-                i += 2;
-                // skip any extra duplicates beyond two
-                while (i < n && nums[i] == nums[i - 1]) i++;
-            }
-            else { // single element
-                nums[k++] = nums[i++];
+        for(int i=2; i<n; i++){
+            if(nums[i]!=nums[k-2]){
+                nums[k]=nums[i];
+                k++;
             }
         }
-
         return k;
+        
     }
 };
