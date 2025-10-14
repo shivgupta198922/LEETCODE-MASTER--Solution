8ed30c1fec74b06a596572df5093e65244918108
shivgupta198922 <153531902+shivgupta198922@users.noreply.github.com>
2025-10-14 22:42:41 +0530

    Time: 2 ms (75.87%), Space: 10.2 MB (54.09%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,51 +1,55 @@
 class Solution {
-public:
-    map<int,int> straightline;
-    map<int,int> diagonallyleft;
-    map<int,int> diagonallyright;
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> output;
-        vector<int> path;
-        solve(n,0,output,path);
-        return output;        
-    }
-
-    void solve(int& n,int row,vector<vector<string>>& output,vector<int> path){
-        if(row==n){
-            make_ans(output,path,n);
-            return;
+    bool issafe(int row, int col, vector<string> &board, int n ){
+        int dr =row;
+        int dc=col;
+        while(row>=0 && col>=0){
+            if(board[row][col]=='Q') return false;
+            row--;
+            col--;
         }
-        for(int i=0;i<n;i++){
-            if(isSafe(row,i,n)){
-                path.push_back(i);
-                straightline[i]++;
-                diagonallyright[row+i]++;
-                diagonallyleft[row-i]++;
-                solve(n,row+1,output,path);
-                straightline[i]--;
-                diagonallyright[row+i]--;
-                diagonallyleft[row-i]--;
-                path.pop_back();
-            }
+        row=dr;
+        col=dc;
+        while(col>=0){
+            if(board[row][col]=='Q') return false;
+            col--;
+            
         }
-        return;
-    }
-
-    bool isSafe(int row,int col,int n){
-        if (straightline[col] || diagonallyright[row + col] || diagonallyleft[row-col]) {
-            return false;
+        
+        row=dr;
+        col=dc;
+        while(row<n && col>=0){
+            if(board[row][col]=='Q') return false;
+            row++;
+            col--;
         }
         return true;
+
+    }
+    void solv(int col, vector<string>& board ,vector<vector<string>> &ans, int n){
+        if(col==n){
+            ans.push_back(board);
+            return;
+        }
+        for(int row=0; row<n; row++){
+            if(issafe(row, col, board, n)){
+                board[row][col]='Q';
+                solv(col+1, board,ans, n);
+                board[row][col]='.';
+            }
+        }
+
     }
 
-    void make_ans(vector<vector<string>>& output,vector<int> path,int n){
-        vector<string> ans;
-        for(auto i:path){
-            string temp(n,'.');
-            temp[i]='Q';
-            ans.push_back(temp);
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n, '.');
+        for(int i=0; i<n; i++){
+            board[i]=s;
         }
-        output.push_back(ans);
-
+        solv(0, board, ans, n);
+        return ans;
+        
     }
 };
